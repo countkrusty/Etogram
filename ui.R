e4918c9fec2648ea13a847fe640beafe085e7d98
countkrusty <tristan.wiessalla@posteo.de>
2019-05-22 16:57:35 +0200

    initial version 0.1, multiple if statements

---
+++ ui.R
@@ -0,0 +1,36 @@
+library(shiny)
+library(ggplot2)
+library(readxl)
+library(colourpicker)
+
+
+shinyUI(
+  
+  
+  pageWithSidebar(
+    
+    headerPanel("Visualise Behaviour"),
+    
+    sidebarPanel(
+      
+      fileInput(inputId = "file", label = "Choose an Excel (.xlsx or .csv) File", 
+                multiple = FALSE, 
+                accept = c("text/xlsx", "text/microsoft-excel-pen-XML-format-spreadsheet-file", ".xlsx",
+                           "text/csv", "text/comma-separated-value", ".csv"),
+                width = NULL, buttonLabel = "Find...",
+                placeholder = "No File selected"
+      ),
+      uiOutput("optionz"),
+      uiOutput("selectize"),
+      uiOutput("colourSelecter"),
+      
+      sliderInput(inputId = "width", label = "width", min = 0.01, max = 0.4, value = 0.2, step = 0.01),
+      # To download the finished graph
+      downloadButton("downloadData", "Download")
+      
+    ),
+    
+    
+    mainPanel(plotOutput("graph"), height = "600px", quoted = TRUE)
+  )
+)
