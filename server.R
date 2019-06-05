9c8975bdc6bd02a61d1c7727218300f83371a9c5
countkrusty <tristan.wiessalla@posteo.de>
2019-06-05 22:10:10 +0200

    cleaned up pdf output, def colour now colour-blind-friendly

--- server.R
+++ server.R
@@ -2,6 +2,7 @@ library(shiny)
 library(ggplot2)
 library(readxl)
 library(colourpicker)
+library(viridis)
 
 
 
@@ -28,7 +29,7 @@ shinyServer(function(input, output, session) {
   
   ##Define the groupCheckBox
   output$optionz <- renderUI({
-    checkboxGroupInput("optionz","Select Behaviour", checkC(), selected = as.vector(as.character(checkC())))
+    checkboxGroupInput("optionz","1) Exclude Behaviour", checkC(), selected = as.vector(as.character(checkC())))
   })
   
   
@@ -38,7 +39,7 @@ shinyServer(function(input, output, session) {
   
   ##The function can't handle blank spaces, that's why I remove them for this bit of the program
   output$selectize <- renderUI({
-    selectizeInput("select","Select:", 
+    selectizeInput("select","2) Import behaviour to select colour", 
                    choices = gsub(" ", "_", as.list(levels(as.factor(unique(na.omit(as.character(as.vector(as.matrix((scorSelecter()[2:ncol(scor1())])))))))))), ##here lies the bug of not recognising empty spaces
                    multiple = TRUE)
   })
@@ -46,11 +47,11 @@ shinyServer(function(input, output, session) {
   output$colourSelecter <-  renderUI ({
     lev <- sort(input$select)
     
-    ###Default is #FFFFFF, even though I set value differently
+    ###Default is selected colour blind-friendly, randomly picked
     lapply(seq_along(lev), function(i) {
       colourInput(inputId = paste0("colour", lev[i]),
                   label = paste0("Choose colour for ", lev[i]),
-                  value = '4166F5',
+                  value = viridis_pal(option = "plasma", begin = runif(1, 0, 1))(i),
                   allowTransparent = TRUE
                   
       )
@@ -68,65 +69,62 @@ shinyServer(function(input, output, session) {
     
     
     ##Plot the data using ggplot
-    PLOT <- reactive({
+    
+    
+    PLOT <- ggplot(scorSelecter(), aes(x = scorSelecter()[,1], y = "", fill = as.factor(scorSelecter()[2:ncol(scor1())])) ) +
       
-      print(
-        
-        #somehow the interactive heigth setting is now muted within the expression
-        ggplot(scorSelecter(), aes(x = scorSelecter()[,1], y = "", fill = as.factor(scorSelecter()[2:ncol(scor1())])) ) +
-          
-        {if(ncol(scor1()) == 2)ylim(-0.2005, 0.2005)} +
-        {if(ncol(scor1()) != 2)scale_y_continuous(breaks = NULL, labels = NULL)} +
-          
-        {if(ncol(scor1()) >= 2)geom_tile(aes(x = scorSelecter()[,1], y = 0, fill = as.factor(scorSelecter()[,2])), scorSelecter(), width = 0.2, height = input$width, size = 2)} + 
-        {if(ncol(scor1()) >= 3)geom_tile(aes(x = scorSelecter()[,1], y = 0.125, fill = as.factor(scorSelecter()[,3])), scorSelecter(), width = 0.2, height = 0.02, size = 2)} +
-        {if(ncol(scor1()) >= 4)geom_tile(aes(x = scorSelecter()[,1], y = -0.125, fill = as.factor(scorSelecter()[,4])), scorSelecter(), width = 0.2, height = 0.02, size = 2)} +
-        {if(ncol(scor1()) >= 5)geom_tile(aes(x = scorSelecter()[,1], y = -0.145, fill = as.factor(scorSelecter()[,5])), scorSelecter(), width = 0.2, height = 0.02, size = 2)} +
-          
-          scale_fill_manual(values = colours, na.translate = FALSE) +
-          
-          ## Set background, name axes, name legend
-          
-          labs(title = "Scored Behaviour ", x = "Time(s)", y = NULL,
-               fill = "Considered Behaviour") +
-          
-          ##Background white, I want lines along the axes
-          
-          
-          
-          theme(panel.border = element_blank(),
-                axis.line.y = element_blank(), 
-                axis.ticks.y = element_blank(),
-                axis.text.y = element_blank(), 
-                panel.grid = element_blank(),
-                panel.grid.minor.x = element_line(size = 0.05),
-                panel.grid.major.x = element_line(size = 0.05),
-                panel.background = element_blank(),
-                axis.line.x = element_line(colour = "black"))
-      )
+    {if(ncol(scor1()) == 2)ylim(-0.2005, 0.2005)} +
+      
+    {if(ncol(scor1()) >= 2)geom_tile(aes(x = scorSelecter()[,1], y = 0, fill = as.factor(scorSelecter()[,2])), scorSelecter(), width = 0.2, height = input$width, size = 2)} + 
+    {if(ncol(scor1()) >= 3)geom_tile(aes(x = scorSelecter()[,1], y = 0.125, fill = as.factor(scorSelecter()[,3])), scorSelecter(), width = 0.2, height = 0.02, size = 2)} +
+    {if(ncol(scor1()) >= 4)geom_tile(aes(x = scorSelecter()[,1], y = -0.125, fill = as.factor(scorSelecter()[,4])), scorSelecter(), width = 0.2, height = 0.02, size = 2)} +
+    {if(ncol(scor1()) >= 5)geom_tile(aes(x = scorSelecter()[,1], y = -0.145, fill = as.factor(scorSelecter()[,5])), scorSelecter(), width = 0.2, height = 0.02, size = 2)} +
+      
+      scale_fill_manual(values = colours, na.translate = FALSE) +
+      
+      ## Set background, name axes, name legend
+      
+      labs(title = "Scored Behaviour ", x = "Time(s)", y = NULL,
+           fill = "Considered Behaviour") +
+      
+      ##Background white, I want lines along the axes
       
       
-    })
-    
-    PLOT()
-    
-    ##To save the finished
-    output$downloadData <- downloadHandler(
-      filename = function() {
-        if (grepl(".csv", input$file$datapath, fixed = TRUE)){
-          paste(gsub(".csv*", "", input$file), 'pdf', sep='.') 
-        }
-        else if (grepl(".xlsx", input$file$datapath, fixed = TRUE)){
-          paste(gsub(".xlsx.*", "", input$file), 'pdf', sep='.') 
-        }
-      },
-      content = function(file) {
-        ggsave(file, plot = PLOT(), device = "pdf", width=10, height=10, unit="in", dpi=600)
-      }
-    )
+      
+      theme(panel.border = element_blank(),
+            axis.line.y = element_blank(), 
+            axis.ticks.y = element_blank(),
+            axis.text.y = element_blank(), 
+            panel.grid = element_blank(),
+            panel.grid.minor.x = element_line(size = 0.05),
+            panel.grid.major.x = element_line(size = 0.05),
+            panel.background = element_blank(),
+            axis.line.x = element_line(colour = "black"))
     
     
-    
+    PLOT
   })
   
-}) 
+  
+  
+  ##To save the finished
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      if (grepl(".csv", input$file$datapath, fixed = TRUE)){
+        paste(gsub(".csv*", "", input$file), 'pdf', sep='.') 
+      }
+      else if (grepl(".xlsx", input$file$datapath, fixed = TRUE)){
+        paste(gsub(".xlsx.*", "", input$file), 'pdf', sep='.') 
+      }
+    },
+    content = function(file) {
+      ggsave(file, device = "pdf", width=10, height=10, unit="in", dpi=600)
+    }
+  )
+  
+  
+  
+})
+
+
+
